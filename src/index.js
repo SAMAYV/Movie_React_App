16faff780eb43e722e9a4d66aa748cf970e1467d
SAMAYV <samay@iitg.ac.in>
2021-07-04 19:52:29 +0530

    Added logger

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ import './index.css';
 import rootReducer from './reducers';
 
 // logger(obj)(next)(action)
-const logger = function({dispatch, getState}) {
-	return function(next) {
-		return function(action){
-			// write middleware code
-			console.log('ACTION TYPE', action.type);
-			next(action);
-		}
-	}
+// const logger = function({dispatch, getState}) {
+// 	return function(next) {
+// 		return function(action){
+// 			// write middleware code
+// 			console.log('ACTION TYPE', action.type);
+// 			next(action);
+// 		}
+// 	}
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+	// write middleware code
+	console.log('ACTION TYPE', action.type);
+	next(action);
 }
 
 const store = createStore(rootReducer, applyMiddleware(logger));
