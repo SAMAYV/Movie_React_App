c4469b9d786be45f73ba482697d3a3e766189810
SAMAYV <samay@iitg.ac.in>
2021-07-04 15:24:29 +0530

    Subscribe to store

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,43 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favorites</div>
-        </div>
+class App extends React.Component {
+	componentDidMount () {
+		const {store} = this.props;
+		store.subscribe(() => {
+			console.log('UPDATED');
+			this.forceUpdate();
+		})
+		// make api call
+		// dispatch action
+		store.dispatch({
+			type: 'ADD_MOVIES',
+			movies: data
+		})
 
-        <div className="list"> 
-            {data.map((movie) => (
-				<MovieCard movie={movie} />
-			))}
-        </div>
-      </div>
-    </div>
-  );
+		console.log(store.getState());
+	}
+	render () {
+		console.log('RENDER');
+		const movies = this.props.store.getState();
+		return (
+			<div className="App">
+			<Navbar />
+			<div className="main">
+				<div className="tabs">
+				<div className="tab">Movies</div>
+				<div className="tab">Favorites</div>
+				</div>
+
+				<div className="list"> 
+					{movies.map((movie, index) => (
+						<MovieCard movie={movie} key={`movies-${index}`} />
+					))}
+				</div>
+			</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './components/App';
 import './index.css';
+import movies from './reducers';
+
+const store = createStore(movies);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
