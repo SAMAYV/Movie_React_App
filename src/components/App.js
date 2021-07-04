c1d615f436c06eafcd2878ff5d2c46d6cc41fe4b
SAMAYV <samay@iitg.ac.in>
2021-07-04 16:27:29 +0530

    Favourites added

--- src/components/App.js
+++ src/components/App.js
@@ -17,9 +17,18 @@ class App extends React.Component {
 
 		console.log(store.getState());
 	}
+	isMovieFavourite = (movie) => {
+        const {favourites} = this.props.store.getState();
+        const index = favourites.indexOf(movie);
+        if(index !== -1){
+            return true;
+        }
+        return false;
+    }
 	render () {
 		console.log('RENDER');
 		const {list}  = this.props.store.getState(); // list, favourites
+		console.log(this.props.store.getState());
 		return (
 			<div className="App">
 			<Navbar />
@@ -31,7 +40,12 @@ class App extends React.Component {
 
 				<div className="list"> 
 					{list.map((movie, index) => (
-						<MovieCard movie={movie} key={`movies-${index}`} />
+						<MovieCard 
+							movie={movie} 
+							key={`movies-${index}`} 
+							dispatch={this.props.store.dispatch} 
+							isFavourite={this.isMovieFavourite(movie)}
+						/>
 					))}
 				</div>
 			</div>
