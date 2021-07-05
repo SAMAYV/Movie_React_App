0922a5b9ad7773bf213d124bbe1be030315aac79
SAMAYV <samay@iitg.ac.in>
2021-07-05 14:19:15 +0530

    some changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -40,7 +40,7 @@ export function addMovieToList(movie) {
 export function handleMovieSearch(movie) {
     const url = `https://omdbapi.com/?apikey=3ca5df7&t=${movie}`;
     return function(dispatch) {
-        fetch(url)
+        fetch(url) // returns a promise
         .then(response => response.json())
         .then(movie => {
             console.log('movie', movie);
